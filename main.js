223285c0af1d29fff819cce8b5bdedf31c17b10b
zuffdaddy <Zakfrazier@gmail.com>
2021-03-18 23:29:57 -0400

    New assets and code

--- main.js
+++ main.js
@@ -64,3 +64,66 @@
   init();
 
 }).call(this);
+
+(function() {
+  var init, rotate, start, stop,
+    active = false,
+    angle = 0,
+    rotation = 0,
+    startAngle = 0,
+    center = {
+      x: 0,
+      y: 0
+    },
+    R2D = 180 / Math.PI,
+    rot = document.getElementById('aob_disc');
+
+  init = function() {
+    rot.addEventListener("mousedown", start, false);
+    $(document).bind('mousemove', function(event) {
+      if (active === true) {
+        event.preventDefault();
+        rotate(event);
+      }
+    });
+    $(document).bind('mouseup', function(event) {
+      event.preventDefault();
+      stop(event);
+    });
+  };
+
+  start = function(e) {
+    e.preventDefault();
+    var bb = this.getBoundingClientRect(),
+      t = bb.top,
+      l = bb.left,
+      h = bb.height,
+      w = bb.width,
+      x, y;
+    center = {
+      x: l + (w / 2),
+      y: t + (h / 2)
+    };
+    x = e.clientX - center.x;
+    y = e.clientY - center.y;
+    startAngle = R2D * Math.atan2(y, x);
+    return active = true;
+  };
+
+  rotate = function(e) {
+    e.preventDefault();
+    var x = e.clientX - center.x,
+      y = e.clientY - center.y,
+      d = R2D * Math.atan2(y, x);
+    rotation = d - startAngle;
+    return rot.style.webkitTransform = "rotate(" + (angle + rotation) + "deg)";
+  };
+
+  stop = function() {
+    angle += rotation;
+    return active = false;
+  };
+
+  init();
+
+}).call(this);
